50f29fd47396ef5f81dede8ba3a242da6953e8a5
AsifKamalTias <asifkamaltias@gmail.com>
2022-06-24 22:10:12 +0600

    migration completed

---
+++ database/migrations/2022_06_24_155216_create_deliveries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->string('delivery_product_name');
+            $table->string('delivery_product_weight');
+            $table->double('delivery_price');
+            $table->string('delivery_destination_address');
+            $table->string('delivery_source_address');
+            $table->string('delivery_contact');
+            $table->integer('client_id');
+            $table->integer('deliveryman_id');
+            $table->string('delivery_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
